491242c6dadbb82cd396ea0ada300785e515f660
root <root@WINDELL-QVPHNA1.localdomain>
2021-08-21 10:48:13 +0100

    added a new paragraph

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
 
         <p>Docker production and nginx ready!</p>
+        <p>Woow, new feature ...</p>
       </header>
     </div>
   );
