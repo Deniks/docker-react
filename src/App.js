4131f996c7fc723b50e63c753802336e9d19f09f
root <root@WINDELL-QVPHNA1.localdomain>
2021-08-21 09:16:18 +0100

    test fix

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
         <p>Docker production and nginx ready!</p>
       </header>
     </div>
